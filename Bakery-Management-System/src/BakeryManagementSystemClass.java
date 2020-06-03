d2fc7f0a307b20735dfebbba9d2ae6c479c1a774
Eshi44 <e.hameliervolino@gmail.com>
2020-06-03 18:34:50 -0400

    added baked good items and cost of item in new parent class as well as created a exit application method

--- Bakery-Management-System/src/BakeryManagementSystemClass.java
+++ Bakery-Management-System/src/BakeryManagementSystemClass.java
@@ -10,6 +10,9 @@ import javax.swing.JSeparator;
 import javax.swing.SwingConstants;
 import javax.swing.border.MatteBorder;
 import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class BakeryManagementSystemClass {
 
@@ -24,6 +27,9 @@ public class BakeryManagementSystemClass {
 	private JTextField textField_7;
 	private JTextField textField_8;
 	private JTextField textField_9;
+	private JTextField textField_10;
+	private JTextField textField_11;
+	private JTextField textField_12;
 
 	/**
 	 * Launch the application.
@@ -76,9 +82,9 @@ public class BakeryManagementSystemClass {
 		panel_1.setBounds(222, 166, 443, 47);
 		frame.getContentPane().add(panel_1);
 		
-		JLabel lblNewLabel_1 = new JLabel("Lemon Drop Bar");
-		panel_1.add(lblNewLabel_1);
-		lblNewLabel_1.setFont(new Font("Times New Roman", Font.BOLD, 30));
+		JLabel Lemon_Drop_Bar = new JLabel("Lemon Drop Bar");
+		panel_1.add(Lemon_Drop_Bar);
+		Lemon_Drop_Bar.setFont(new Font("Times New Roman", Font.BOLD, 30));
 		
 		textField = new JTextField();
 		textField.setFont(new Font("Tahoma", Font.PLAIN, 20));
@@ -90,9 +96,9 @@ public class BakeryManagementSystemClass {
 		panel_2.setBounds(222, 224, 443, 47);
 		frame.getContentPane().add(panel_2);
 		
-		JLabel lblCupcake = new JLabel("Vanilla Cupcake");
-		lblCupcake.setFont(new Font("Times New Roman", Font.BOLD, 30));
-		panel_2.add(lblCupcake);
+		JLabel Vanilla_Cupcake = new JLabel("Vanilla Cupcake");
+		Vanilla_Cupcake.setFont(new Font("Times New Roman", Font.BOLD, 30));
+		panel_2.add(Vanilla_Cupcake);
 		
 		textField_1 = new JTextField();
 		textField_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
@@ -104,9 +110,9 @@ public class BakeryManagementSystemClass {
 		panel_3.setBounds(222, 282, 443, 47);
 		frame.getContentPane().add(panel_3);
 		
-		JLabel lblCake = new JLabel("Chocolate Cake");
-		lblCake.setFont(new Font("Times New Roman", Font.BOLD, 30));
-		panel_3.add(lblCake);
+		JLabel Chocolate_Cake = new JLabel("Chocolate Cake");
+		Chocolate_Cake.setFont(new Font("Times New Roman", Font.BOLD, 30));
+		panel_3.add(Chocolate_Cake);
 		
 		textField_2 = new JTextField();
 		textField_2.setFont(new Font("Tahoma", Font.PLAIN, 20));
@@ -118,9 +124,9 @@ public class BakeryManagementSystemClass {
 		panel_4.setBounds(222, 340, 443, 47);
 		frame.getContentPane().add(panel_4);
 		
-		JLabel lblMintOreoCake = new JLabel("Mint Oreo Cake");
-		lblMintOreoCake.setFont(new Font("Times New Roman", Font.BOLD, 30));
-		panel_4.add(lblMintOreoCake);
+		JLabel Mint_Oreo_Cake = new JLabel("Mint Oreo Cake");
+		Mint_Oreo_Cake.setFont(new Font("Times New Roman", Font.BOLD, 30));
+		panel_4.add(Mint_Oreo_Cake);
 		
 		textField_3 = new JTextField();
 		textField_3.setFont(new Font("Tahoma", Font.PLAIN, 20));
@@ -132,9 +138,9 @@ public class BakeryManagementSystemClass {
 		panel_5.setBounds(222, 398, 443, 47);
 		frame.getContentPane().add(panel_5);
 		
-		JLabel lblGooeyBrownies = new JLabel("Gooey Brownies");
-		lblGooeyBrownies.setFont(new Font("Times New Roman", Font.BOLD, 30));
-		panel_5.add(lblGooeyBrownies);
+		JLabel Gooey_Brownies = new JLabel("Gooey Brownies");
+		Gooey_Brownies.setFont(new Font("Times New Roman", Font.BOLD, 30));
+		panel_5.add(Gooey_Brownies);
 		
 		textField_4 = new JTextField();
 		textField_4.setFont(new Font("Tahoma", Font.PLAIN, 20));
@@ -146,9 +152,9 @@ public class BakeryManagementSystemClass {
 		panel_6.setBounds(686, 166, 443, 47);
 		frame.getContentPane().add(panel_6);
 		
-		JLabel lblBaguette = new JLabel("Banana Pudding");
-		lblBaguette.setFont(new Font("Times New Roman", Font.BOLD, 30));
-		panel_6.add(lblBaguette);
+		JLabel Banana_Pudding = new JLabel("Banana Pudding");
+		Banana_Pudding.setFont(new Font("Times New Roman", Font.BOLD, 30));
+		panel_6.add(Banana_Pudding);
 		
 		textField_5 = new JTextField();
 		textField_5.setFont(new Font("Tahoma", Font.PLAIN, 20));
@@ -160,9 +166,9 @@ public class BakeryManagementSystemClass {
 		panel_7.setBounds(686, 398, 443, 47);
 		frame.getContentPane().add(panel_7);
 		
-		JLabel lblCottagePudding = new JLabel("Cottage Pudding");
-		lblCottagePudding.setFont(new Font("Times New Roman", Font.BOLD, 30));
-		panel_7.add(lblCottagePudding);
+		JLabel Cottage_Pudding = new JLabel("Cottage Pudding");
+		Cottage_Pudding.setFont(new Font("Times New Roman", Font.BOLD, 30));
+		panel_7.add(Cottage_Pudding);
 		
 		textField_6 = new JTextField();
 		textField_6.setFont(new Font("Tahoma", Font.PLAIN, 20));
@@ -174,9 +180,9 @@ public class BakeryManagementSystemClass {
 		panel_8.setBounds(686, 340, 443, 47);
 		frame.getContentPane().add(panel_8);
 		
-		JLabel lblBostonCreamPie = new JLabel("Boston Cream Pie");
-		lblBostonCreamPie.setFont(new Font("Times New Roman", Font.BOLD, 28));
-		panel_8.add(lblBostonCreamPie);
+		JLabel Boston_Cream_Pie = new JLabel("Boston Cream Pie");
+		Boston_Cream_Pie.setFont(new Font("Times New Roman", Font.BOLD, 28));
+		panel_8.add(Boston_Cream_Pie);
 		
 		textField_7 = new JTextField();
 		textField_7.setFont(new Font("Tahoma", Font.PLAIN, 20));
@@ -188,9 +194,9 @@ public class BakeryManagementSystemClass {
 		panel_9.setBounds(686, 282, 443, 47);
 		frame.getContentPane().add(panel_9);
 		
-		JLabel lblCinnamonRoll = new JLabel("Cinnamon Rolls");
-		lblCinnamonRoll.setFont(new Font("Times New Roman", Font.BOLD, 30));
-		panel_9.add(lblCinnamonRoll);
+		JLabel Cinnamon_Rolls = new JLabel("Cinnamon Rolls");
+		Cinnamon_Rolls.setFont(new Font("Times New Roman", Font.BOLD, 30));
+		panel_9.add(Cinnamon_Rolls);
 		
 		textField_8 = new JTextField();
 		textField_8.setFont(new Font("Tahoma", Font.PLAIN, 20));
@@ -202,13 +208,74 @@ public class BakeryManagementSystemClass {
 		panel_10.setBounds(686, 224, 443, 47);
 		frame.getContentPane().add(panel_10);
 		
-		JLabel lblSourdough = new JLabel("Ice Cream Cake");
-		lblSourdough.setFont(new Font("Times New Roman", Font.BOLD, 30));
-		panel_10.add(lblSourdough);
+		JLabel Ice_Cream_Cake = new JLabel("Ice Cream Cake");
+		Ice_Cream_Cake.setFont(new Font("Times New Roman", Font.BOLD, 30));
+		panel_10.add(Ice_Cream_Cake);
 		
 		textField_9 = new JTextField();
 		textField_9.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		textField_9.setColumns(12);
 		panel_10.add(textField_9);
+		
+		JPanel panel_11 = new JPanel();
+		panel_11.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		panel_11.setBounds(222, 520, 443, 47);
+		frame.getContentPane().add(panel_11);
+		
+		JLabel lblCost = new JLabel("Total         ");
+		lblCost.setFont(new Font("Times New Roman", Font.BOLD, 30));
+		panel_11.add(lblCost);
+		
+		textField_10 = new JTextField();
+		textField_10.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		textField_10.setColumns(12);
+		panel_11.add(textField_10);
+		
+		JPanel panel_12 = new JPanel();
+		panel_12.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		panel_12.setBounds(222, 581, 443, 47);
+		frame.getContentPane().add(panel_12);
+		
+		JLabel lblTax = new JLabel("Tax            ");
+		lblTax.setFont(new Font("Times New Roman", Font.BOLD, 30));
+		panel_12.add(lblTax);
+		
+		textField_11 = new JTextField();
+		textField_11.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		textField_11.setColumns(12);
+		panel_12.add(textField_11);
+		
+		JPanel panel_13 = new JPanel();
+		panel_13.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		panel_13.setBounds(222, 639, 443, 47);
+		frame.getContentPane().add(panel_13);
+		
+		JLabel label_1 = new JLabel("Total Cost");
+		label_1.setFont(new Font("Times New Roman", Font.BOLD, 30));
+		panel_13.add(label_1);
+		
+		textField_12 = new JTextField();
+		textField_12.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		textField_12.setColumns(12);
+		panel_13.add(textField_12);
+		
+		JButton btnNewButton = new JButton("Total");
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 25));
+		btnNewButton.setBounds(686, 602, 142, 84);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnClear = new JButton("Clear");
+		btnClear.setFont(new Font("Tahoma", Font.BOLD, 25));
+		btnClear.setBounds(838, 602, 142, 84);
+		frame.getContentPane().add(btnClear);
+		
+		JButton btnExit = new JButton("Exit");
+		btnExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnExit.setFont(new Font("Tahoma", Font.BOLD, 25));
+		btnExit.setBounds(987, 602, 142, 84);
+		frame.getContentPane().add(btnExit);
 	}
 }
---
+++ Bakery-Management-System/src/Parent_Bakery.java
@@ -0,0 +1,53 @@
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class Parent_Bakery {
+	
+	public double itemCost;
+	
+	public double Lemon_Drop_Bar;
+	public double Vanilla_Cupcake;
+	public double Chocolate_Cake;
+	public double Gooey_Brownies;
+	public double Banana_Pudding;
+	public double Cottage_Pudding;
+	public double Boston_Cream_Pie;
+	public double Cinnamon_Rolls;
+	public double Ice_Cream_Cake;
+
+	
+	
+	public double pLemon_Drop_Bar = 3.25;
+	public double pVanilla_Cupcake = 2.90;
+	public double pChocolate_Cake = 9.35;
+	public double pGooey_Brownies = 13.50;
+	public double pBanana_Pudding = 3.25;
+	public double pCottage_Pudding = 2.50;
+	public double pBoston_Cream_Pie = 10.25;
+	public double pCinnamon_Rolls = 3.75;
+	public double pIce_Cream_Cake = 4.25;
+	
+	public double GetTotal()
+	{
+		itemCost=Lemon_Drop_Bar + Vanilla_Cupcake + Chocolate_Cake + Gooey_Brownies + Banana_Pudding + Cottage_Pudding + Boston_Cream_Pie + Cinnamon_Rolls + Ice_Cream_Cake;
+
+		return itemCost;
+	}
+	
+	
+	private JFrame frame;
+	
+	
+//	exit application method
+	public void iExitManagementSystem() {
+		frame = new JFrame();
+		if (JOptionPane.showConfirmDialog(frame, "Confirm you wish to exit", "Bakery Management System"),
+				JOptionPane.YES_NO_OPTION) == JOptionPane.YES_NO_OPTION) {
+					System.exit(0);
+				}
+	}
+}
+
+
+
+
